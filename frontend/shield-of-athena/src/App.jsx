0f6bd7aeb632809c9cc728e72cdbf9c1dceff872
mildshield14 <80240232+mildshield14@users.noreply.github.com>
2025-11-14 14:09:35 -0500

    timeline feature + i18n
    # Conflicts:
    #	frontend/shield-of-athena/package.json
    #	frontend/shield-of-athena/src/App.js

---
+++ frontend/shield-of-athena/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ImpactJourney from "./components/ImpactJourney";
+
+function App() {
+    return (
+        <div className="App">
+            <ImpactJourney />
+        </div>
+    );
+}
+
+export default App;
